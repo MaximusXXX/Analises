44e987c423e813302990591a69d33e84d5144ad3
MaximusXXX <josielvasconcelosconsultor@hotmail.com>
2016-05-07 17:57:50 -0400

    Update main.cpp

--- Work/main.cpp
+++ Work/main.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
+    int inicio,fim,resultado;
+    inicio = clock();
     Arquivo_Test("dados.txt");// aqui estou gerando meus arquivos 
     Merge_Sort_Externo("dados.txt");//aqui estou chamando minha função mergesortesxterno que vai ordenar o arquivo
     Verificar("dados.txt");
-    
+    fim = clock();
+    resultado = fim-inicio;
+    printf("%d",resultado);
     system("pause>nul");
     return 0;
 }
